5b8a16ee8bb06f736e504009666fd5efa35b75e7
Abdul-Rehman143 <ab4987937gmail.com>
2025-02-25 04:42:39 +0500

    appsheet

---
+++ precache-manifest.6c879daa3e5af96927492ecadadb7f10.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d0401bdb9f3928f3fb76",
+    "url": "/app-sheet-project/static/js/runtime~main.dd2628d0.js"
+  },
+  {
+    "revision": "9ba33b5931699a2e1429",
+    "url": "/app-sheet-project/static/js/main.6408cb24.chunk.js"
+  },
+  {
+    "revision": "4421e4efa02cafe289af",
+    "url": "/app-sheet-project/static/js/2.b1c09b02.chunk.js"
+  },
+  {
+    "revision": "9ba33b5931699a2e1429",
+    "url": "/app-sheet-project/static/css/main.ae48c49d.chunk.css"
+  },
+  {
+    "revision": "4421e4efa02cafe289af",
+    "url": "/app-sheet-project/static/css/2.14cc03f7.chunk.css"
+  },
+  {
+    "revision": "0b7d3aecbfaecd7a011e5202c7d696a8",
+    "url": "/app-sheet-project/index.html"
+  }
+];
